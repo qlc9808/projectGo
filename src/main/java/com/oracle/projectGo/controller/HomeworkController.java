e353f0c194e0090a499e97bca56bddee53c26b16
andylee-dev <andylee.devknight@gmail.com>
2023-12-06 12:30:07 +0900

    [feat] homework controller getList

--- src/main/java/com/oracle/projectGo/controller/HomeworkController.java
+++ src/main/java/com/oracle/projectGo/controller/HomeworkController.java
@@ -1,11 +1,20 @@
 package com.oracle.projectGo.controller;
 
+import com.oracle.projectGo.dto.Homeworks;
+import com.oracle.projectGo.service.Paging;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -14,17 +23,26 @@ import java.util.UUID;
 @RequestMapping(value = "/homework")
 public class HomeworkController {
     @RequestMapping(value = "listHomework")
-    public String listHomework(String currentPage, Model model) {
+    public String listHomework(Homeworks homework, String currentPage, Model model) {
         UUID transactionId = UUID.randomUUID();
         try {
             log.info("[{}]{}:{}:{}", transactionId, this.getClass().getSimpleName(),"listHomework", "start");
 
-            /* TODO: */
+            /* TODO: GET TOTAL HOMEWORK COUNT  */
 
-            /* TODO: */
-
-            /* TODO: */
-
+            /* TODO: PAGING PROCESS */
+            Paging page = new Paging(30, currentPage);
+            homework.setStart(page.getStart());
+            homework.setEnd(page.getEnd());
+            /* TODO: GET HOMEWORK LIST */
+            List<Homeworks> hwList = new ArrayList<Homeworks>();
+            for (int i = 0; i < 10; i++) {
+                Homeworks hw = new Homeworks();
+                hw.setId(i);
+                hw.setTitle("title"+i);
+                hwList.add(hw);
+            }
+            /* TODO: SET ATTRIBUTE */
 
         } catch (Exception e) {
             log.error("[{}]{}:{}:{}", transactionId, this.getClass().getSimpleName(), "listHomework", e.getMessage());
@@ -32,5 +50,26 @@ public class HomeworkController {
             log.info("[{}]{}:{}:{}", transactionId,  this.getClass().getSimpleName(), "listHomework", "end");
         }
         return "educate/homework/listHomework";
+    }
+
+    @RequestMapping(value = "getRecommendations", method = RequestMethod.POST)
+    public ResponseEntity getRecommendations(@RequestBody Homeworks homework) {
+        UUID transactionId = UUID.randomUUID();
+        try {
+            log.info("[{}]{}:{}:{}", transactionId, this.getClass().getSimpleName(), "listHomework", "start");
+
+            /* TODO: */
+
+            /* TODO: */
+
+            /* TODO: */
+            return ResponseEntity.ok().build();
+
+        } catch (Exception e) {
+            log.error("[{}]{}:{}:{}", transactionId, this.getClass().getSimpleName(), "listHomework", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } finally {
+            log.info("[{}]{}:{}:{}", transactionId, this.getClass().getSimpleName(), "listHomework", "end");
+        }
     }
 }
