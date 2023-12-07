2f67f830a423ce82a5e2dda9ebc6efc7065511ac
andylee-dev <andylee.devknight@gmail.com>
2023-12-07 17:40:07 +0900

    [feat] getHomeworksList

--- src/main/java/com/oracle/projectGo/dto/Homeworks.java
+++ src/main/java/com/oracle/projectGo/dto/Homeworks.java
@@ -1,5 +1,6 @@
 package com.oracle.projectGo.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 import java.util.Date;
@@ -10,7 +11,8 @@ public class Homeworks {
     private int    userId;            // 회원번호
     private String title;             // 숙제명
     private String content;           // 숙제내용
-    private int    progress;          // 진도
+    @Min(value = 0, message = "Progress must be a positive number.")
+    private Integer     progress;          // 진도
     private Date   deadline;          // 제출기한
     private Date   distributionDate;  // 전송일자
     private Date   createdAt;         // 생성일
