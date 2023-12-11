168765f9026393cb1ebbc4bf211c3d4a9268825d
andylee-dev <andylee.devknight@gmail.com>
2023-12-11 18:13:20 +0900

    [fix] security

--- src/main/java/com/oracle/projectGo/configuration/SecurityConfig.java
+++ src/main/java/com/oracle/projectGo/configuration/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.oracle.projectGo.configuration;
 
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,26 +20,26 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return ((web) -> web.ignoring()
-                .requestMatchers("/static/**", "/h2-console/**"));
+                .requestMatchers("/resources/**", "/h2-console/**"));
     }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((authorizeRequests) ->authorizeRequests
                 .requestMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .permitAll());
+                .anyRequest().permitAll()
+        );
+        http.formLogin(form -> form
+            .loginPage("/login")
+            .permitAll()
+            .defaultSuccessUrl("/")
+        );
+      http.logout(logout -> logout
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true)
+            .permitAll());
 
         return http.build();
     }
