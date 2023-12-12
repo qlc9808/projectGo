21d24b5e05e28ce34b57fb2f1259b1ec72c44731
andylee-dev <andylee.devknight@gmail.com>
2023-12-12 14:07:17 +0900

    [feat] add Homework Enum

--- src/main/java/com/oracle/projectGo/type/HomeworksEvaluateType.java
+++ src/main/java/com/oracle/projectGo/type/HomeworksEvaluateType.java
@@ -5,6 +5,24 @@ package com.oracle.projectGo.type;
  * @params fdasdfdsfsad
  */
 public enum HomeworksEvaluateType {
-//    comment
+    NONE("미평가",0),
+    POOR("미흡", 1),
+    AVERAGE("보통", 2),
+    EXCELLENT("우수", 3);
 
+    private String label;
+    private int value;
+
+    HomeworksEvaluateType(String label, int value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
